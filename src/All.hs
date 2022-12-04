227c05c271e19f7732cc193ccbcf7fb66543c909
Bruno da Rocha Paiva <brunomipaiva@gmail.com>
2022-12-04 13:16:13 +0000

    Day 4

--- src/All.hs
+++ src/All.hs
@@ -3,9 +3,11 @@ module All (printAll) where
 import Day01
 import Day02
 import Day03
+import Day04
 
 printAll :: IO ()
 printAll = do
   printDay01
   printDay02
   printDay03
+  printDay04
---
+++ src/Day04.hs
@@ -0,0 +1,54 @@
+module Day04 (printDay04, haveOverlap)  where
+
+import Text.Read (readMaybe)
+import Data.Function ((&))
+
+import Control.Arrow ((***), (&&&), second)
+import Control.Monad.Zip (mzip)
+
+haveContainment :: ((Int, Int), (Int, Int)) -> Bool
+haveContainment = or
+                . uncurry fmap
+                . ( (&) &&& const
+                    [ uncurry (&&) . ((uncurry (<=) . (fst *** fst)) &&& (uncurry (>=) . (snd *** snd)))
+                    , uncurry (&&) . ((uncurry (>=) . (fst *** fst)) &&& (uncurry (<=) . (snd *** snd)))
+                    ]
+                  )
+
+haveOverlap :: ((Int, Int), (Int, Int)) -> Bool
+haveOverlap = not . or
+            . uncurry fmap
+            . ( (&) &&& const
+                [ uncurry (<) . (snd *** fst)
+                , uncurry (>) . (fst *** snd)
+                ]
+              )
+
+part1 :: String -> Maybe Int
+part1 = fmap (length . filter haveContainment)
+      . traverse
+        ( uncurry mzip
+        . ((uncurry mzip . (readMaybe *** readMaybe)) *** (uncurry mzip . (readMaybe *** readMaybe)))
+        . ((second tail . span (/='-')) *** (second tail . span (/='-') . tail))
+        . span (/=',')
+        )
+      . lines
+
+part2 :: String -> Maybe Int
+part2 = fmap (length . filter haveOverlap)
+      . traverse
+        ( uncurry mzip
+        . ((uncurry mzip . (readMaybe *** readMaybe)) *** (uncurry mzip . (readMaybe *** readMaybe)))
+        . ((second tail . span (/='-')) *** (second tail . span (/='-') . tail))
+        . span (/=',')
+        )
+      . lines
+
+printDay04 :: IO ()
+printDay04 = do
+  file <- readFile "data/day04"
+  putStrLn "-- DAY 04 --"
+  putStr "  Part 1: "
+  putStrLn $ maybe "something went wrong :(" show $ part1 file
+  putStr "  Part 2: "
+  putStrLn $ maybe "something went wrong :(" show $ part2 file
